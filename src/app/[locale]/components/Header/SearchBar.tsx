b70adb0758c6023a3455f0b9603b1c1ba6f45ade
seemix <91602641+seemix@users.noreply.github.com>
2026-01-27 23:48:25 +0200

    logo link added

--- src/app/[locale]/components/Header/SearchBar.tsx
+++ src/app/[locale]/components/Header/SearchBar.tsx
@@ -1,22 +1,21 @@
 'use client';
 
 import { TfiSearch } from 'react-icons/tfi';
+
 import { useModal } from '@/app/[locale]/hooks/useModal';
 import { SearchComponent } from '@/app/[locale]/components/index';
 
 const SearchBar = () => {
-    // const t = await getTranslations('Header');
     const { showModal } = useModal();
 
     return (
         <button type={'button'} className={'cursor-pointer'}
-                onClick={() => {showModal(<SearchComponent/>, 'zoom');
-        }}>
+                onClick={() => {
+                    showModal(<SearchComponent/>, 'zoom');
+                }}>
             <TfiSearch
                 size={25}
-                className={
-                    'text-black transition-colors duration-300 hover:text-[var(--main)]'
-                }
+                className={'text-black transition-colors duration-300 hover:text-[var(--main)]'}
             />
         </button>
     );
