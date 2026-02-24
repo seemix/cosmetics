0b27876fb45336149affa5fbe11698d99eeafc20
seemix <91602641+seemix@users.noreply.github.com>
2026-02-24 13:41:49 +0200

    pre-deploy 1.1012

--- src/app/[locale]/assets/assets.ts
+++ src/app/[locale]/assets/assets.ts
@@ -1,6 +1,6 @@
 export const assets = {
 	currency: 'MDL',
-	backendUrl: process.env.BACKEND_URL || 'http://http://195.178.106.119',
+	backendUrl: process.env.BACKEND_URL || 'https://admin.nextlevelshop.md',
 	backendUrl2: 'http://localhost:3000',
 	phone: '+37361033551',
 	instagramLink: 'https://www.instagram.com/',
