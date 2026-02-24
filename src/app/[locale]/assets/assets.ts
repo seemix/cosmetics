6a48dff5cf62959f312c24ff5f4b04aec994f057
seemix <91602641+seemix@users.noreply.github.com>
2026-02-24 11:25:56 +0200

    pre-deploy 1.1011

--- src/app/[locale]/assets/assets.ts
+++ src/app/[locale]/assets/assets.ts
@@ -1,7 +1,7 @@
 export const assets = {
 	currency: 'MDL',
-	backendUrl2: process.env.BACKEND_URL || 'http://http://195.178.106.119',
-	backendUrl: 'http://localhost:3000',
+	backendUrl: process.env.BACKEND_URL || 'http://http://195.178.106.119',
+	backendUrl2: 'http://localhost:3000',
 	phone: '+37361033551',
 	instagramLink: 'https://www.instagram.com/',
 	telegramLink: 'https://www.telegram.com/',
