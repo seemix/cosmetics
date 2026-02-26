47f3af4e3ba0445c5bc5bf2b979a975e0433a9e9
seemix <91602641+seemix@users.noreply.github.com>
2026-02-27 00:25:10 +0200

    try to fix token issue 1.02

--- src/app/[locale]/(protected)/layout.tsx
+++ src/app/[locale]/(protected)/layout.tsx
@@ -8,7 +8,6 @@ export default async function ProtectedRoutes({ children }: { children: React.Re
     const { backendUrl } = assets;
     const cookieStore = await cookies();
     const token = cookieStore.get('payload-token')?.value;
-    console.log(`token= ${token}`);
 
     const res = await fetch(`${backendUrl}/api/users/me`, {
         headers: {
--- src/app/[locale]/components/ProductCard/ProductCard.tsx
+++ src/app/[locale]/components/ProductCard/ProductCard.tsx
@@ -27,7 +27,6 @@ export default function ProductCard({ product, index = 0, labels = true }: {
         action,
         bestSeller
     } = product;
-    console.log(wholesalePrice);
     const { backendUrl, currency } = assets;
     const t = useTranslations('Catalog');
     const price = wholesalePrice || retailPrice;
