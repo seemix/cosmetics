138c2c78a3351b383d41a46ff41cf3d48667b404
seemix <91602641+seemix@users.noreply.github.com>
2026-03-08 22:49:09 +0200

    fixed reset password form

--- src/app/[locale]/components/SetNewPassword/SetNewPassword.tsx
+++ src/app/[locale]/components/SetNewPassword/SetNewPassword.tsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { useTranslations } from 'next-intl';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { createNewPasswordSchema } from '@/app/[locale]/components/SetNewPassword/newPasswordSchema';
-import { ErrorComponent, Loader, PasswordResetSuccess } from '@/app/[locale]/components';
+import { ErrorComponent, FormInput, Loader, PasswordResetSuccess } from '@/app/[locale]/components';
 import { useAuthStore } from '@/app/[locale]/stores/auth.store';
 
 export default function SetNewPassword({ token }: { token: string }) {
@@ -30,46 +30,17 @@ export default function SetNewPassword({ token }: { token: string }) {
                         {t('inputNewPassword')}
                     </h2>
                     <div className={'space-y-3'}>
-                        <div>
-                            <label htmlFor={'password'} className={'block text-xs font-medium'}>
-                                {t('password')}
-                            </label>
-                            <input
-                                id={'password'}
-                                type={'password'}
-                                {...register('password')}
-                                className={`mt-1 w-full border border-gray-300 px-3 py-2 focus:outline-none focus:ring-1 
-                                    focus:ring-black text-sm ${errors.password ? 'border-red-500 focus:ring-red-500' : ''}`}
-                            />
-                            <div className={'h-5'}>
-                                {errors.password && (
-                                    <p className={'mt-1 text-xs text-red-600'}>
-                                        {errors.password.message}
-                                    </p>
-                                )}
-                            </div>
-                        </div>
+                        <FormInput label={t('password')}
+                                   register={register('password')}
+                                   id={'password'}
+                                   type={'password'}
+                                   error={errors.password}/>
                         {/* Confirm password */}
-                        <div>
-                            <label htmlFor={'confirmPassword'} className={'block text-xs font-medium'}>
-                                {t('confirmPassword')}
-                            </label>
-                            <input
-                                id={'confirmPassword'}
-                                type={'password'}
-                                {...register('confirmPassword')}
-                                className={`mt-1 w-full border border-gray-300 px-3 py-2 focus:outline-none focus:ring-1 
-                                    focus:ring-black text-sm 
-                                    ${errors.confirmPassword ? 'border-red-500 focus:ring-red-500' : ''}`}
-                            />
-                            <div className={'h-5'}>
-                                {errors.confirmPassword && (
-                                    <p className={'mt-1 text-xs text-red-600'}>
-                                        {errors.confirmPassword.message}
-                                    </p>
-                                )}
-                            </div>
-                        </div>
+                        <FormInput label={t('confirmPassword')}
+                                   register={register('confirmPassword')}
+                                   id={'confirmPassword'}
+                                   type={'password'}
+                                   error={errors.confirmPassword}/>
                         <button type={'submit'}
                                 disabled={loading}
                                 className={`cursor-pointer transition-colors duration-300 border-1 border-black  
