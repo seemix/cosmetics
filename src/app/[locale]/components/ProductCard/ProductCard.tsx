5fad46e83fc482bfbe208c5912ae7da5dd811dab
seemix <91602641+seemix@users.noreply.github.com>
2026-03-10 09:57:23 +0200

    made card appear animation faster

--- src/app/[locale]/components/ProductCard/ProductCard.tsx
+++ src/app/[locale]/components/ProductCard/ProductCard.tsx
@@ -36,9 +36,9 @@ export default function ProductCard({ product, index = 0, labels = true }: {
 
     return (
         <motion.div
-            initial={{ opacity: 0, x: 25, filter: 'blur(10px)' }}
+            initial={{ opacity: 0, x: 15, filter: 'blur(10px)' }}
             whileInView={{ opacity: 1, x: 0, filter: 'blur(0px)' }}
-            transition={{ duration: .35, ease: 'easeInOut', delay: index * .25 }}
+            transition={{ duration: .2, ease: 'easeInOut', delay: index * .15 }}
             viewport={{ once: true }}
             className={`bg-white shadow-[0_2px_12px_rgba(0,0,0,0.1)] w-full max-w-[320px] min-w-[250px] mx-auto
                         grid grid-rows-[auto_1fr_auto] h-full`}>
