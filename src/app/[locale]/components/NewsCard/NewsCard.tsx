cd6e1dacb289456d40469a3a0adfe9ec65fe0f8b
seemix <91602641+seemix@users.noreply.github.com>
2026-03-10 20:02:41 +0200

    fixend news view

--- src/app/[locale]/components/NewsCard/NewsCard.tsx
+++ src/app/[locale]/components/NewsCard/NewsCard.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import { getTranslations } from 'next-intl/server';
 
 import type { PostCard } from '@/app/[locale]/types/post-card';
 import { assets } from '@/app/[locale]/assets/assets';
@@ -7,8 +6,8 @@ import Link from 'next/link';
 
 export default async function NewsCard({ post, locale }: { post: PostCard, locale: string }) {
 
-    const t = await getTranslations('StaticPages');
-    const { title, slide, excerpt, slug } = post;
+  //  const t = await getTranslations('StaticPages');
+    const { title, slide, slug } = post;
     const { backendUrl } = assets;
     const date = new Date(post.createdAt);
     const formattedDate = new Intl.DateTimeFormat(locale, {
@@ -21,23 +20,25 @@ export default async function NewsCard({ post, locale }: { post: PostCard, local
                 <p className={'text-gray-500 text-right text-sm'}>{formattedDate}</p>
                 <h3 className={'text-xl text-center mb-4 font-semibold'}>{title}</h3>
             </div>
-            <div className={'grid md:grid-cols-[auto_1fr] gap-4'}>
-                <div className={'w-full aspect-video md:w-70 relative my-1'}>
+            {/*<div className={'grid md:grid-cols-[auto_1fr] gap-4'}>*/}
+            <Link href={`news//${slug}`}>
+                <div className={'w-full aspect-[1920/620] relative my-1'}>
                     <Image src={`${backendUrl}${slide.sizes.medium.url}`} alt={`${slide.alt} || postImage`}
                            className={'object-cover object-center'}
                            fill/>
                 </div>
-                <p>{excerpt}</p>
-            </div>
-            <div className={'w-full flex justify-end -mt-3'}>
-                <Link href={`news//${slug}`}>
-                <button type={'button'}
-                        className={`border border-black p-2 transition-colors hover:border-[var(--main)] 
-                                        hover:text-[var(--main)] cursor-pointer mt-4 mb-3`}>
-                    {t('readMore')}
-                </button>
-                </Link>
-            </div>
+            </Link>
+            {/*<p>{excerpt}</p>*/}
+            {/*</div>*/}
+            {/*<div className={'w-full flex justify-end -mt-3'}>*/}
+            {/*    <Link href={`news//${slug}`}>*/}
+            {/*<button type={'button'}*/}
+            {/*        className={`border border-black p-2 transition-colors hover:border-[var(--main)] */}
+            {/*                        hover:text-[var(--main)] cursor-pointer mt-4 mb-3`}>*/}
+            {/*    {t('readMore')}*/}
+            {/*</button>*/}
+            {/*</Link>*/}
+            {/*</div>*/}
         </div>
     );
 }
