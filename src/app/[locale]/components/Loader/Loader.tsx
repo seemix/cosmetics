a9470b85773dd2e03a8bb323cddf244a9b5ee39b
seemix <91602641+seemix@users.noreply.github.com>
2026-03-11 11:51:26 +0200

    fixed loader

--- src/app/[locale]/components/Loader/Loader.tsx
+++ src/app/[locale]/components/Loader/Loader.tsx
@@ -1,7 +1,7 @@
 export default function Loader() {
     return (
-        <div className={`h-6 w-6 mx-auto animate-spin rounded-full border-2 border-solid border-gray-300] 
-                        border-r-transparent`}>
+        <div className={`h-4 w-4 sm:h-6 sm:w-6 mx-auto animate-spin rounded-full border-1 sm:border-2 
+                        border-solid border-gray-300] border-r-transparent`}>
             <span className={'sr-only'}>Loading...</span>
         </div>
     );
