37e7bc683f3eb2d9b32c21160e9e89d6332e465f
seemix <91602641+seemix@users.noreply.github.com>
2026-03-11 15:08:16 +0200

    made a menu on a button on the phone 1.03605

--- src/app/[locale]/components/Header/HeaderLogo.tsx
+++ src/app/[locale]/components/Header/HeaderLogo.tsx
@@ -1,9 +1,18 @@
 import { Logo } from '@/app/[locale]/components';
+import Link from 'next/link';
+import { assets } from '@/app/[locale]/assets/assets';
+import { FaPhoneSquareAlt } from 'react-icons/fa';
 
 export default function HeaderLogo() {
   return (
-    <div className={'w-full flex sm:hidden p-2 bg-foreground justify-center'}>
+    <div className={'w-full flex sm:hidden p-2 bg-foreground justify-between items-center px-3'}>
       <Logo/>
+        <Link href={`tel:${assets.phone}`}
+              className={`text-dark flex items-center text-sm transition-colors duration-300 
+                                            hover:text-[var(--main)]`}>
+            <FaPhoneSquareAlt className={'w-6 h-auto mr-1'} />
+            <p>{assets.phone}</p>
+        </Link>
     </div>
   );
 }
