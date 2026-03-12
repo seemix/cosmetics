2919940dd4e407bedf778067675681ce98d7926b
seemix <91602641+seemix@users.noreply.github.com>
2026-03-12 19:28:55 +0200

    fixed heading menu styling on phone

--- src/app/[locale]/components/Menu/HeadMenu.tsx
+++ src/app/[locale]/components/Menu/HeadMenu.tsx
@@ -8,28 +8,27 @@ import { useModal } from '@/app/[locale]/hooks/useModal';
 
 export default function HeadMenu() {
 
-	const t = useTranslations('StaticPages');
-	const { hideModal } = useModal();
+    const t = useTranslations('StaticPages');
+    const { hideModal } = useModal();
 
-	return (
-		<nav className={`flex gap-10 w-full max-w-130 pt-1 justify-end text-md text-dark 
+    return (
+        <nav className={`flex gap-10 w-full max-w-130 pt-1 justify-end text-md text-dark 
 		                font-(family-name:--font-roboto)`}>
-			<ul className={'flex flex-wrap w-full justify-evenly'}>
-				{headMenuData.map((item) => (
-					<li key={item.name}>
-						<Link
-							onClick={hideModal}
-							data-bubble={'true'}
-							href={`${item.link}`}
-							className={`whitespace-nowrap text-sm p-2 text-dark transition-colors duration-300 
-                                            tracking-normal sm:px-0 sm:py-0 sm:border-none sm:bg-transparent 
-                                            hover:text-[var(--main)]`}
-						>
-							{t(item.name)}
-						</Link>
-					</li>
-				))}
-			</ul>
-		</nav>
-	);
+            <ul className={'flex flex-wrap w-full justify-evenly gap-x-1 gap-y-6'}>
+                {headMenuData.map((item) => (
+                    <li key={item.name}>
+                        <Link
+                            onClick={hideModal}
+                            data-bubble={'true'}
+                            href={`${item.link}`}
+                            className={`whitespace-nowrap text-md sm:text-sm text-dark transition-colors py-3 sm:py-1
+            							duration-300 tracking-normal hover:text-[var(--main)] inline-block`}
+                        >
+                            {t(item.name)}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 }
