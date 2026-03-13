1e376f783b06f16c53d0cb7d0a04cf81bc30e951
seemix <91602641+seemix@users.noreply.github.com>
2026-03-14 00:02:44 +0200

    complex fixing to main page 1.02

--- src/app/[locale]/components/RelatedProducts/RelatedProducts.tsx
+++ src/app/[locale]/components/RelatedProducts/RelatedProducts.tsx
@@ -13,10 +13,11 @@ import type { IProduct } from '@/app/[locale]/types/product';
 export default function RelatedProducts({ products, labels }: { products: IProduct[], labels: boolean }) {
     return (
         <Swiper
-            className={'max-w-[95%] my-4'}
+            className={'max-w-[98%] my-4'}
             spaceBetween={20}
             navigation={true}
             breakpoints={{
+                320: { slidesPerView: 1.5 },
                 400: { slidesPerView: 1.8 },
                 600: { slidesPerView: 3.3 },
                 768: { slidesPerView: 3.9 },
@@ -25,7 +26,7 @@ export default function RelatedProducts({ products, labels }: { products: IProdu
         >
             {products.map((product,index) => <SwiperSlide key={product.id}
                                                                         className={'flex h-auto'}>
-             <div className={'h-119 w-60'}><ProductCard product={product} index={index} labels={labels}/></div>
+             <div className={'h-116'}><ProductCard product={product} index={index} labels={labels}/></div>
             </SwiperSlide>)}
         </Swiper>
     );
