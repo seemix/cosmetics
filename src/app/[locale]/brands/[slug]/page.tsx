cc6cba7a278cb05881854a7d07cd7426820a2b86
seemix <91602641+seemix@users.noreply.github.com>
2026-03-17 19:40:21 +0200

    added brand products

--- src/app/[locale]/brands/[slug]/page.tsx
+++ src/app/[locale]/brands/[slug]/page.tsx
@@ -1,21 +1,28 @@
 import { getTranslations } from 'next-intl/server';
 
-import { propsType } from '@/app/[locale]/types/server-component-params';
+import type { propsType } from '@/app/[locale]/types/server-component-params';
 import { assets } from '@/app/[locale]/assets/assets';
-import { BreadCrumbs, StaticPage } from '@/app/[locale]/components';
+import { BreadCrumbs, RelatedProducts, StaticPage } from '@/app/[locale]/components';
 
 export default async function BrandPage(props: propsType) {
     const t = await getTranslations('CatalogMenu');
     const { slug, locale } = await props.params;
     const { backendUrl } = assets;
-    const brand = await fetch(`${backendUrl}/api/brands/${slug}?locale=${locale}`).then(res => res.json());
+    const brand = await fetch(`${backendUrl}/api/brands/${slug}?locale=${locale}`)
+        .then(res => res.json());
+    const { docs } = await fetch(`${backendUrl}/api/products/products-brand${slug}?locale=${locale}`)
+        .then(res => res.json());
     const breadCrumbs =
-        [{ id: '1', title: t('brands'), slug: 'brands' },
-            { id: brand?.id, title: brand?.title, slug: brand?.slug }];
+        [
+            { id: '1', title: t('brands'), slug: 'brands' },
+            { id: brand?.id, title: brand?.title, slug: brand?.slug }
+        ];
 
     return (<div className={'w-full flex max-w-[1100px] p-4 flex-col gap-3'}>
-                <BreadCrumbs breadcrumbs={breadCrumbs}/>
+            <BreadCrumbs breadcrumbs={breadCrumbs}/>
             <StaticPage content={brand?.description} title={brand?.title}/>
+            {/*<h2 className={'font-semibold text-xl text-center'}>{t('bestsellers')}</h2>*/}
+            <RelatedProducts products={docs} labels={true}/>
         </div>
     );
 }
