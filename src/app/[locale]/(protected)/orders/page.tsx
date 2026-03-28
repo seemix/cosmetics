60820da77f1c2af7702f99758d6f4a4ab04e00ce
seemix <91602641+seemix@users.noreply.github.com>
2026-03-28 21:31:57 +0200

    tryed to fix orders locale

--- src/app/[locale]/(protected)/orders/page.tsx
+++ src/app/[locale]/(protected)/orders/page.tsx
@@ -4,16 +4,17 @@ import { getTranslations } from 'next-intl/server';
 
 import { BreadCrumbs, MyOrders } from '@/app/[locale]/components';
 import { assets } from '@/app/[locale]/assets/assets';
+import type { propsType } from '@/app/[locale]/types/server-component-params';
 
-export default async function OrdersPage() {
-
+export default async function OrdersPage(props: propsType) {
+    const { locale } = await props.params;
     const t = await getTranslations('Account');
     const breadCrumbs = [
         { id: '0', title: t('account'), slug: 'main' },
         { id: '1', title: t('orders'), slug: 'orders' }
     ];
     const cookieStore = await cookies();
-    const { docs } = await fetch(`${assets.backendUrl}/api/orders/my`,{
+    const { docs } = await fetch(`${assets.backendUrl}/api/orders/my?locale=${locale}`,{
         headers: {
             Cookie: cookieStore.toString()
         },
